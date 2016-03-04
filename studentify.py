e3593235f24c1d5d86204abf7d691de76eeede32
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2016-03-04 19:34:30 +0100

    Reapply same license as previously (MPLv2) to the new studentify.py

--- studentify.py
+++ studentify.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python2
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # check python version newer than 2.7
 import sys
 v = sys.version_info
