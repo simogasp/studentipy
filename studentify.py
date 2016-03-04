591cd74892ed0dd5bb67ba4d0fa9bb3f9a55a771
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2016-03-04 19:22:40 +0100

    Replacing original studentify by new one.

--- studentify.py
+++ studentify.py
@@ -1,182 +1,175 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# check python version newer than 2.7
+import sys
+v = sys.version_info
+assert v.major > 2 or (v.major == 2 and v.minor >= 7), "Minimal version required is 2.7"
 
-import sys, getopt, os, re, shutil
-import argparse
+# useful imports
+import argparse, os, shutil, tempfile
+from functools import partial
 from collections import namedtuple
 
 langInfo = namedtuple('langInfo', 'name, extensions, token, startToken, endToken')
 suppLang = [];
 suppLang.append(langInfo('c/c++', ['.c','.cpp','.h','.hpp','.cc', '.cxx'],'//!!', '//<!!','//>!!'))
-suppLang.append(langInfo('matlab', ['.m'],'\%\%!!', '\%\%<!!','\%\%>!!'))
+suppLang.append(langInfo('matlab', ['.m'],'%%!!', '%%<!!','%%>!!'))
 suppLang.append(langInfo('javascript', ['.js'],'//!!', '//<!!','//>!!'))
 suppLang.append(langInfo('python', ['.py'],'#!!', '#<!!','#>!!'))
 suppLang.append(langInfo('java', ['.java'],'//!!', '//<!!','//>!!'))
 
 
+def studentify_main(args):
+    """ Studentify the files given in arguments.
 
-def help(name):
-    """ print the usage for the program
+        args is a namespace containing at least the following variables
+            func: (function) this very function
+            input: ([string]) the files/folder to studentify
+            output: (None or string) the output file/folder
+
+        all other variables in the namespace (for new features)
+        are going to be stored en the "flags" dictionary.
+
+        we have 3 basic cases depending on the output variable:
+        None   -> Modify files and/or folders in place
+        file   -> Input must contain only one file
+        folder -> Copy inputs in this folder
     """
-    print('Usage:\n\t'+name+' -i <input file or dir> -o <output file or dir>')
-    print("\nSupported languages:\nLanguage\t\ttoken\t\tstartToken\t\tendToken")
-    for i in suppLang:
-        print(i.name+"\t\t\t"+i.token+"\t\t"+i.startToken+"\t\t"+i.endToken)
+    outPath = args.output
+    inPaths = args.input
+    # flags is the dictionary containing all other flags
+    flags = {k:v for k,v in args.__dict__.iteritems() if k not in ['func','input','output']}
 
-
-
-def removeCode(inputfilename, outputfilename, token, startToken, endToken):
-    """ create a copy of a file by removing the line of code ended by a comment containing the token
-         inputfilename is the file to copy
-         outputfilename is the destination file
-         token is the commentary token to detect in order to remove the line    
-    """
-
-    if os.path.isfile(inputfilename):
-
-        # any token without text in front, the token can be follow by text
-        pattern = re.compile(r"[ \t]*" + token +".*[ \t]*$", re.UNICODE)
-        # any tokenr without text in front, the token can be follow by text
-        multiStart = re.compile(r'^[ \t]*'+startToken+'.*[ \t]*', re.UNICODE)
-        # any token without text in front, the token can be follow by text
-        multiEnd = re.compile(r'^[ \t]*'+endToken+'.*[ \t]*', re.UNICODE)
-        # open the file
-        with open(inputfilename) as fp:
-            # create the new file
-            dstfile = open(outputfilename,'w')
-
-            # used to keep track of multiline 
-            multi = False
-
-            # parse line by line
-            for line in fp:
-
-                # if a start token has not been detected recently
-                if not multi:
-
-                    #try to detect a start token
-                    s = multiStart.search(line)
-
-                    if s == None:
-
-                        # if the line do not match copy it to the destination file
-                        r = pattern.search(line)
-                        if r == None:
-                            dstfile.write(line)
-                        else:
-                            dstfile.write('\n')
-
-                    else:
-                        print("Detected start token:"+line)
-                        multi = True;
-                        # do nothing
+    if outPath == None:
+        if not flags['nobackup']:
+            backupPath = os.path.abspath("studentify_backup")
+            if flags['debug']: print("backing up files in: " + backupPath)
+            os.makedirs(backupPath)
+            for i in inPaths:
+                if os.path.isfile(i):
+                    shutil.copy(i, backupPath)
                 else:
-
-                    #try to detect a end token
-                    s = multiEnd.search(line)
-
-                    if s == None:
-                        # do not copy the code and just add an empty line
-                        dstfile.write('\n')
-                    else:
-                        # otherwise do nothing as we don't want to copy the line neither, 
-                        # just add an empty line
-                        multi = False
-                        print("Detected end token:"+line)
-                    # print line
-            
-            #close the files
-            dstfile.close();
-
-
-
-def parseDirectory(inputDir, outputDir):
-    """ parse the input directory recursively and recreate the same repository
-        in output parsing the files and removing the lines tagged with the proper token   
-    """
-    # create the output directory
-    os.makedirs(outputDir)
-
-    # list the elements in the input directory
-    for dirname, dirnames, filenames in os.walk(inputDir):
-        # print("\nDirectories in "+dirname)
-        newbase = os.path.normpath(re.compile('^'+inputDir).sub(outputDir+os.sep,dirname))+os.sep
-        # print newbase
-        # print path to all subdirectories first.
-        for subdirname in dirnames:
-            # create the a copy of the directory
-            os.makedirs(os.path.normpath(os.path.join(newbase, subdirname)))
-            print(os.path.normpath(os.path.join(newbase, subdirname)))
-        # print path to all filenames.
-        # print("Filenames")
-        for filename in filenames:
-            # print os.path.normpath(os.path.join(newbase, filename))
-            n, ext = os.path.splitext(filename)
-            m = [x for x in suppLang if ext in x.extensions]
-            if not m:
-                print("No supported language found for file "+filename)
-                shutil.copyfile(os.path.join(dirname, filename), os.path.normpath(os.path.join(newbase, filename)))
-            else:
-                # if it is a known language apply the transformation with the given token
-                print("Detected "+m[0].name+" language for "+filename+"\tProcessing...")
-                removeCode(os.path.join(dirname, filename), os.path.normpath(os.path.join(newbase, filename)), m[0].token, m[0].startToken, m[0].endToken)
-            
-
-
-
-
-
-if __name__ == "__main__":       
-
-    parser = argparse.ArgumentParser(description='A simple script to generate theversion of a code to be given to the student.'+
-                    'The script parses the input file and create a copy of it removing the lines tagged with a special comment tag.'+
-                    'It automatically detects the language. More over it can be used on a directory to recursively create a copy of it parsing all the files in it.')
-    parser.add_argument('-i', '--inputFile', required=True, help='The input file or directory to parse')
-    parser.add_argument('-o', '--outputFile', required=True, help='The output file or directory')
-    args = parser.parse_args()         
-
-    # parse the input arguments
-    inputfile = args.inputFile
-    outputfile = args.outputFile
-
-    os.path.normpath(inputfile)
-    os.path.normpath(outputfile)
-    # check the input arguments
-    if inputfile == outputfile:
-        print("Can't use the same file as input and output")
-        sys.exit()
-
-    print('Input is "', inputfile)
-    print('Output is "', outputfile)
-
-    # if it's a folder
-    if os.path.isdir(inputfile):
-        print("Parsing directory is not available (yet)")
-        # check whether the outdir already exist
-        if os.path.exists(outputfile):
-            # if so ask the user what to do (delete or stop)
-            answer = input("The output directory "+outputfile+" already exists. Do you want to remove it? [Y/n]: ")
-            if answer.lower() in ["y", "yes"] or answer == "":
-                shutil.rmtree(outputfile)
-            else:
-                print("Aborting...")
-                sys.exit()
-        parseDirectory(inputfile, outputfile) 
-
-    # otherwise if it is a file
-    elif os.path.isfile(inputfile):
-        # get the extension
-        base, ext = os.path.splitext(inputfile)
-        m = [x for x in suppLang if ext in x.extensions]
-        if not m:
-            print("No supported language found for file "+inputfile)
-            sys.exit()
-        else:
-            # if it is a known language apply the transformation with the given token
-            print("Detected "+m[0].name+" language for "+inputfile+"\nProcessing...\n")
-            removeCode(inputfile, outputfile, m[0].token, m[0].startToken, m[0].endToken)
+                    shutil.copytree(i, os.path.join(backupPath, os.path.basename(i)))
+            if flags['debug']: print("backup done.")
+            print("if you do not want backup, use the --nobackup flags")
+        for i in inPaths:
+            isFile = os.path.isfile(i)
+            studentify_one(i, i, isFile, flags)
+    elif len(inPaths)==1:
+        isFile = os.path.isfile(outPath) if os.path.exists(outPath) else os.path.isfile(inPaths[0])
+        studentify_one(inPaths[0], outPath, isFile, flags)
     else:
-        print('Cannot find the input folder/file!')
+        studentify_multiple(inPaths, outPath, flags)
+
+def studentify_one(inputPath, outputPath, outputIsFile, flags):
+    inputPath = os.path.abspath(inputPath)
+    outputPath = os.path.abspath(outputPath)
+    # if we studentify in place
+    if inputPath == outputPath:
+        if os.path.isfile(inputPath):
+            assert outputIsFile, outputPath + " is actually an existing file"
+            if flags['debug']: print(inputPath + " -> " + inputPath)
+            processFile(inputPath, flags)
+        if os.path.isdir(inputPath):
+            assert not outputIsFile, outputPath + " is actually an existing directory"
+            lst = os.listdir(inputPath)
+            inputPaths = [os.path.join(inputPath, i) for i in lst]
+            studentify_multiple(inputPaths, outputPath, flags)
+    # if the input is a file, it depends on if output is a file also
+    elif os.path.isfile(inputPath):
+        if outputIsFile:
+            outputDir = os.path.dirname(outputPath)
+            if not os.path.exists(outputDir):
+                os.makedirs(outputDir)
+            shutil.copy(inputPath, outputPath)
+            if flags['debug']: print(inputPath + " -> " + outputPath)
+            processFile(outputPath, flags)
+        else:
+            outputFile = os.path.join(outputPath, os.path.basename(inputPath))
+            studentify_one(inputPath, outputFile, True, flags)
+    # else the input is a folder
+    else:
+        assert not outputIsFile
+        lst = os.listdir(inputPath)
+        inputPaths = [os.path.join(inputPath, i) for i in lst]
+        newOutputDir = os.path.join(outputPath, os.path.basename(inputPath))
+        studentify_multiple(inputPaths, newOutputDir, flags)
+
+def studentify_multiple(inputPaths, outputDir, flags):
+    for i in inputPaths:
+        studentify_one(i, outputDir, False, flags)
+
+def processFile(filePath, flags):
+    """ Process a file to remove lines containing some token.
+
+    filePath must be an absolute path.
+
+    1. Check if file is to be processed (matching filtypes in suppLang)
+    2. Open a temporary file
+    3. Process each line and write result in temporary file
+    4. Rename temporary file into original file
+    """
+    assert os.path.isabs(filePath), "use an absolute path instead: " + filePath
+    base, ext = os.path.splitext(filePath)
+    l = [lang for lang in suppLang if ext in lang.extensions]
+    if not l:
+        if flags['debug']: print("No supported language found for file " + filePath)
+    else:
+        lang = l[0]
+        # open a temporary file
+        with open(filePath, 'r+b') as f, tempfile.NamedTemporaryFile(delete=False) as ftemp:
+            tempPath = ftemp.name
+            inCommentBlock = False
+            # process each line of the file
+            for line in f:
+                newLine, inCommentBlock = processLine(
+                        line, lang.token, lang.startToken, lang.endToken, inCommentBlock,
+                        flags)
+                ftemp.write(newLine)
+        # rename temp file into original
+        shutil.copystat(filePath, tempPath)
+        shutil.move(tempPath, filePath)
+
+def processLine(line, token, startToken, endToken, inCommentBlock, flags):
+    """ Search for the token in the line.
+    """
+    newLine = line
+    replacementLine = "\n" if not flags['remove'] else ""
+    if inCommentBlock:
+        newLine = replacementLine
+        inCommentBlock = not endToken in line
+    else:
+        inCommentBlock = startToken in line
+        tokenDetected = token in line
+        if inCommentBlock or tokenDetected:
+            newLine = replacementLine
+    return newLine, inCommentBlock
+
+# check that a path (file or folder) exists or not and return it
+def checkPath(path, shouldExist):
+    path = os.path.normpath(path)
+    if shouldExist != os.path.exists(path):
+        msg = "path " + ("does not" if shouldExist else "already") + " exist: " + path
+        raise argparse.ArgumentTypeError(msg)
+    return path
+
+# arguments configuration
+parser = argparse.ArgumentParser()
+parser.set_defaults(func=studentify_main)
+parser.add_argument('-v', '--version', action='version', version='2.0')
+parser.add_argument('input', type=partial(checkPath, shouldExist=True), nargs='+',
+        help='file or folder to studentify')
+parser.add_argument('-o', '--output', type=partial(checkPath, shouldExist=False),
+        help='output file or folder (if input is a folder or contains more than 1 file, this must be a folder)')
+parser.add_argument('-d', '--debug', action='store_true',
+        help='activate debug mode')
+parser.add_argument('--remove', action='store_true',
+        help='remove lines instead of keeping empty lines')
+parser.add_argument('--nobackup', action='store_true',
+        help='do not create backup when studentifying in place')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    args.func(args)
+    #flags = {k:v for k,v in args.__dict__.iteritems() if k not in ['func','input','output']}
+    #print(flags)
